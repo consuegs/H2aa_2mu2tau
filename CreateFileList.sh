a94aaf61fc1d5260427117baa48246e9a2d40c10
Consuegra Sandra <sandra.consuegra.rodriguez@desy.de>
2018-09-24 15:51:40 +0200

    Script to create file lists of Data and MC used in the analysis

---
+++ CreateFileList.sh
@@ -0,0 +1,61 @@
+#QCD:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-15to20_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-20to30_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-20to30_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-30to50_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-30to50_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-50to80_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-50to80_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-80to120_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-120to170_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-170to300_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-170to300_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/user/rasp/ntuples/QCD_Summer16/QCD_Pt-300to470_MuEnrichedPt5_13TeV_pythia8/*root > QCD_Pt-300to470_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/user/rasp/ntuples/QCD_Summer16/QCD_Pt-470to600_MuEnrichedPt5_13TeV_pythia8/*root > QCD_Pt-470to600_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/user/rasp/ntuples/QCD_Summer16/QCD_Pt-600to800_MuEnrichedPt5_13TeV_pythia8/*root > QCD_Pt-600to800_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/user/rasp/ntuples/QCD_Summer16/QCD_Pt-800to1000_MuEnrichedPt5_13TeV_pythia8/*root > QCD_Pt-800to1000_MuEnrichedPt5_13TeV
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13TeV_pythia8/*root > QCD_Pt-20toInf_MuEnrichedPt15_13TeV
+
+#Single top:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8/*.root > ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powheg
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/ST_t-channel_top_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8/*.root > ST_t-channel_top_4f_inclusiveDecays_13TeV-powheg
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8/*.root > ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_ext/*.root > ST_tW_top_5f_inclusiveDecays_13TeV-powheg
+
+#Ttbar:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/*.root > TT_13TeV-powheg-pythia8
+
+#W+jets:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext/*.root > WJetsToLNu_13TeV-madgraphMLM
+
+#Diboson production:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/WW_TuneCUETP8M1_13TeV-pythia8_ext/*.root > WW_13TeV-pythia8
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/WZ_TuneCUETP8M1_13TeV-pythia8_ext1/*.root > WZ_13TeV-pythia8
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/ZZ_TuneCUETP8M1_13TeV-pythia8_ext1/*.root > ZZ_13TeV-pythia8
+
+#Drell Yan:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/*.root > DYJetsToLL_M-10to50_13TeV-madgraphMLM
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1/*.root > DYJetsToLL_M-50_13TeV-madgraphMLM
+
+#Signal samples:
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-3p6_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-3p6
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-4_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-4
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM_mumutautau_ntuples/M5/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-5
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-6_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-6
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM_mumutautau_ntuples/M7/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-7
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-8_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-8
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM_mumutautau_ntuples/M9/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-9
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-10_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-10
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM_mumutautau_ntuples/M11/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-11
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-12_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-12
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM_mumutautau_ntuples/M13/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-13
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGGH_HToAA_AToTauTau_M-14_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-14
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM_mumutautau_ntuples/M15/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-15
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-17_13TeV_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-17
+ls /nfs/dust/cms/user/rasp/ntuples/NMSSM/SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-19_13TeV_madgraph/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-19
+
+#Data:
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016B-03Feb2017_ver2-v2/DoubleMuon/*.root > Run2016B-03Feb2017_ver2-v2
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016C-03Feb2017-v1/DoubleMuon/*.root > Run2016C-03Feb2017-v1
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016D-03Feb2017-v1/DoubleMuon/*.root > Run2016D-03Feb2017-v1
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016E-03Feb2017-v1/DoubleMuon/*.root > Run2016E-03Feb2017-v1
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016F-03Feb2017-v1/DoubleMuon/*.root > Run2016F-03Feb2017-v1
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016G-03Feb2017-v1/DoubleMuon/*.root > Run2016G-03Feb2017-v1
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016H-03Feb2017_ver2-v1/DoubleMuon/*.root > Run2016H-03Feb2017_ver2-v1
+ls /nfs/dust/cms/group/higgs-kit/NMSSM_Haa_8025_v2/DoubleMuon/Run2016H-03Feb2017_ver3-v1/DoubleMuon/*.root > Run2016H-03Feb2017_ver3-v1
