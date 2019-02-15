2f775895ecd336891e6e2b970591794ab6d6ca53
consuegs <sandra.consuegra.rodriguez@desy.de>
2019-02-15 23:46:45 +0100

    Create TMVA_Ploter.sh
    Script to generate plots using the TMVA GUI in batch mode

---
+++ TMVA_Ploter.sh
@@ -0,0 +1,5 @@
+root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM_IsoMu27/MVA_BDT/ma5/test.root", "InputVariables_Id", "")'
+root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM_IsoMu27/MVA_BDT/ma5/test.root", "InputVariables_Deco", "")'
+root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM_IsoMu27/MVA_BDT/ma5/test.root", "InputVariables_PCA", "")'
+root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM_IsoMu27/MVA_BDT/ma5/test.root", "InputVariables_Gauss_Deco", "")'
+root -l -b -q TMVA_Ploter.C
