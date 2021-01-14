3fcfffb1f490b50884b25f5c7bdf9e68f8d7b643
Sandra Consuegra Rodriguez <sandra.consuegra.rodriguez@desy.de>
2021-01-14 19:29:06 +0100

    Updating repository after long time

--- TMVA_Ploter.sh
+++
@@ -1,5 +0,0 @@
-root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM/MVA_BDT/ma5/test.root", "InputVariables_Id", "")'
-root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM/MVA_BDT/ma5/test.root", "InputVariables_Deco", "")'
-root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM/MVA_BDT/ma5/test.root", "InputVariables_PCA", "")'
-root -l -b -q -e 'TMVA::variables("/nfs/dust/cms/user/consuegs/Run/Run2017/H2aa/H2aa_2mu2tau/H2aa_2mu2tau_SM/MVA_BDT/ma5/test.root", "InputVariables_Gauss_Deco", "")'
-root -l -b -q TMVA_Ploter.C
--- check_diskusage.sh
+++
@@ -1,4 +0,0 @@
-#Create monthly disk space report of common validation area 
-du -hs * | sort -h > log_diskusage_$(date +"%Y%m%d").txt
-fs listquota -human >> log_diskusage_$(date +"%Y%m%d").txt
-
